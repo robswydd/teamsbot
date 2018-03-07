fc5c4d9bf96085ca1b9f54fd4bee90c5de9cfaa7
Rob Gallagher <Rob.Gallagher@shropshire.gov.uk>
2018-03-07 11:04:38 +0000

    nitial commit

---
+++ TestBot/app.js
@@ -0,0 +1,23 @@
+'use strict';
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: 'd1a6e543-dc20-42dd-beb3-b563fb453a62',
+    appPassword: 'sxvrmoUSWQ684|({zVZM05_'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
